0c3d4183debc79124702e37c572a93c728b6a90c
WarlordGandhi24 <reevelood@gmail.com>
2019-10-26 18:27:35 -0500

    form start and css changes

--- ultraulta/src/Landing.js
+++ ultraulta/src/Landing.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import {Col,Row,Container} from 'react-bootstrap';
+import {Col,Row,Container,Form, Button} from 'react-bootstrap';
 import SearchField from './SearchField';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 import Header from './Header.js';
 function Landing(props){
 
     return(
-        <div>
+      <div class="yes">
         <Container>
-        <Header/>
+          <Header/>
+        <br></br><br></br><br></br><br></br><br></br><br></br>
+        <Row>
+
+        <Col>
+        </Col>
+        <Col id="hi">
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Enter a YouTube link to start searching for products.</Form.Label>
+            <Form.Control type="email" placeholder="ex: http://youtube.com/watch?v=" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        </Col>
+        <Col>
+        </Col>
+        </Row>
+        <Row>
+        </Row>
         </Container>
         </div>
     );
