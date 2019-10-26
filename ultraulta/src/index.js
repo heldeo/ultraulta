61ad9fe2b6cb28c48a2c26f6fea2869976bd7c00
WarlordGandhi24 <reevelood@gmail.com>
2019-10-26 15:44:06 -0500

    bug fix

--- ultraulta/src/index.js
+++ ultraulta/src/index.js
@@ -5,11 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter,Route,Switch} from 'react-router-dom';
 import Landing from './Landing';
-<<<<<<< HEAD
-import Results from './Results';
-=======
 import Results   from './Results';
->>>>>>> 719aa239b443e668ac0800d21d295bcfeac77886
 
 ReactDOM.render(
     <BrowserRouter>
