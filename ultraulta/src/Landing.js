7cc1731ad795465985a5f1d2de12b3f4491f3eb0
heldeo <heldeo3@hotmail.com>
2019-10-26 14:20:12 -0500

    routing and basic structure implemented

---
+++ ultraulta/src/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Col,Row,Container} from 'react-bootstrap';
+
+
+
+function Landing(props){
+
+    return(
+        <div>
+        <Container>
+
+        </Container>
+        </div>
+    );
+}
+export default Landing;
--- ultraulta/src/index.js
+++ ultraulta/src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import Landing from './Landing'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+ <App>
+    <Switch>
+        <Route exact path = "/" component={Landing}/>
+        <Route exact path = '/results' component={Results}/>
+    </Switch>
+</App>,
+
+</BrowserRouter>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
