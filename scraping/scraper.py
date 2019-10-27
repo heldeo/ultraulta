10a03d2f60d373dab709cda9d12c4f0c0866c401
jeremy-laier <jeremylaier@outlook.com>
2019-10-27 05:53:12 -0500

    handles list of words

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -1,3 +1,6 @@
+import json
+import re
+from flask_cors import CORS, cross_origin
 from flask import Flask
 from flask import request
 from flask import Response
@@ -5,9 +8,8 @@ from flask import abort
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from flask import jsonify
-from flask_cors import CORS, cross_origin
-import re
-import json
+import random
+import time
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
@@ -18,7 +20,7 @@ def recommended():
     # productSearch = str(request.args.get('product'))
     youtube = request.form.get('url')
     print(youtube)
-    resp = getRecommendedProducts("lipstick")
+    resp = getAllProducts(["lipstick", "lipgloss"])
     if len(resp) is 0:
         # print("bad request on: " youtub+ productSearch)
         abort(400)
@@ -26,11 +28,19 @@ def recommended():
 
 
 class Product:
-    def __init__(self, name, price, url, pUrl):
+    def __init__(self, name, price, url, pUrl, time):
         self.name = name
         self.price = price
         self.picUrl = url
         self.productUlr = pUrl
+        self.time = time
+
+
+def getAllProducts(listOfProducts):
+    l = []
+    for p in listOfProducts:
+        l.append(getRecommendedProducts(p))
+    return l
 
 
 def getRecommendedProducts(s):
@@ -47,6 +57,8 @@ def getRecommendedProducts(s):
     pics = []
     url = []
     products = []
+    random.seed(int(time.time()))
+
     for product in soup.findAll("div", {"class": "productQvContainer"}):
         for p in product.findAll("div", {"class": "prod-title-desc"}):
             stringProduct = p.contents[1].text.strip(
@@ -64,9 +76,15 @@ def getRecommendedProducts(s):
             break
 
     for i in range(0, len(names)):
-        p = Product(names[i], prices[i], pics[i], url[i])
-        o = json.dumps(p.__dict__)
-        m = json.loads(o)
-        products.append(m)
+        p = Product(names[i], prices[i], pics[i], url[i], str(
+            random.randint(1, 100*i+1) % 10) + ":" + str(random.randint(10, 59)))
+        products.append(p)
 
-    return products[:8]
+    ret = []
+    for i in sorted(products, key=lambda pro: pro.time):
+        o = json.dumps(i.__dict__)
+        m = json.loads(o)
+        ret.append(m)
+
+    return ret
+    # return random.choices(ret, k=8)
