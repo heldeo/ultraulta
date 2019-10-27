988d8fce72621a60a3fc83994f4885fe0fb33cc7
jeremy-laier <jeremylaier@outlook.com>
2019-10-27 00:43:05 -0500

    added url

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -5,27 +5,31 @@ from flask import abort
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from flask import jsonify
+from flask_cors import CORS, cross_origin
 import re
 import json
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 
 @app.route('/api/recommended')
 def recommended():
-    productSearch = str(request.args.get('product'))
-    resp = getRecommendedProducts(productSearch)
+    # productSearch = str(request.args.get('product'))
+    youtube = request.form.get('url')
+    resp = getRecommendedProducts("lipstick")
     if len(resp) is 0:
-        print("bad request on: " + productSearch)
+        # print("bad request on: " youtub+ productSearch)
         abort(400)
     return json.dumps(resp)
 
 
 class Product:
-    def __init__(self, name, price, url):
+    def __init__(self, name, price, url, pUrl):
         self.name = name
         self.price = price
         self.picUrl = url
+        self.productUlr = pUrl
 
 
 def getRecommendedProducts(s):
@@ -40,6 +44,8 @@ def getRecommendedProducts(s):
     names = []
     prices = []
     pics = []
+    url = []
+    products = []
     for product in soup.findAll("div", {"class": "productQvContainer"}):
         for p in product.findAll("div", {"class": "prod-title-desc"}):
             stringProduct = p.contents[1].text.strip(
@@ -52,8 +58,11 @@ def getRecommendedProducts(s):
         for p in product.findAll("div", {"class": "quick-view-prod"}):
             pics.append(str(re.search(
                 "(?P<url>https?://[^\s'\"]+)", str(p.contents)).group("url")))
-    products = []
+        for p in product.find_all('a', href=True):
+            url.append("https://ulta.com" + p['href'])
+            break
+
     for i in range(0, len(names)):
-        p = Product(names[i], prices[i], pics[i])
+        p = Product(names[i], prices[i], pics[i], url[i])
         products.append(json.dumps(p.__dict__))
-    return products
+    return products[:8]
